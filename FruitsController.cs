3e63d7aaa14c6a194ed0f0a4889e3d13a6b1ccc5
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-08-12 22:27:13 +0530

    Create FruitsController.cs

---
+++ FruitsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_web_api2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsController : ControllerBase
+    {
+
+
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Mango","Grapes",
+            "Gauva"
+        };
+
+
+        [HttpGet]
+        public List<String> getFruits()
+        {
+            return fruits;
+        }
+        [HttpGet("{id}")]
+        public string getFruitsById(int id)
+        {
+            var f = fruits.ElementAt(id);
+            return f;
+
+        }
+        [HttpDelete("{id}")]
+        public void deleteFruits(int id)
+        {
+            fruits.RemoveAt(id);
+
+        }
+    }
+}
